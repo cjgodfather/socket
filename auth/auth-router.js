84aa92a5aa6b4dcaaebd7395953a652c106c4b9a
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-01-05 15:14:40 -0500

    added user login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,30 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", (req, res) => {
   const newUser = req.body;
-  console.log(newUser);
+
   User.create(newUser)
-    .then(r => {
-      const token = jwt.sign({ id: r._id }, process.env.JWT_KEY);
-      return token;
+    .then(user => {
+      console.log(user);
+      const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
+      return { ...user._doc, token };
     })
-    .then(token => res.status(200).json(token))
+    .then(user => res.status(200).json({ user }))
     .catch(err => res.status(500).json(err));
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .select("+password")
+    .then(user => {
+      if (bcrypt.compare(user.password, password)) {
+        res.status(200).json({ message: "successfully login" });
+      }
+    });
 });
 
 router.get("/", (req, res) => {
