7fb6953132cd3b75a58ca384efde0e24d3a1f106
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-01-05 14:06:51 -0500

    added user password encryption

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -10,7 +10,11 @@ router.post("/register", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  User.find().then(users => res.status(200).json(users));
+  User.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,6 +21,12 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+UserSchema.pre("save", async function() {
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", UserSchema);
