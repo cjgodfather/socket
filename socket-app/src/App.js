4403c671b0dcee693f4c91e047e992b901440956
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-01-07 23:04:24 -0500

    added state to register

--- socket-app/src/App.js
+++ socket-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
--- socket-app/src/auth/Register.js
+++ socket-app/src/auth/Register.js
@@ -1,27 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Register = props => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    password_again: "",
+    email: ""
+  });
+
+  console.log(user);
+  const inputChangeHandler = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
   return (
     <form>
       <div>
         <label>Username:</label>
-        <input type="text" name="username" placeholder="username" />
+        <input
+          type="text"
+          name="username"
+          value={user.username}
+          onChange={inputChangeHandler}
+        />
       </div>
       <div>
         <label>Password:</label>
-        <input type="password" name="password" placeholder="password" />
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={inputChangeHandler}
+        />
       </div>
       <div>
         <label>Password again:</label>
         <input
           type="password"
           name="password_again"
-          placeholder="password_again"
+          value={user.password_again}
+          onChange={inputChangeHandler}
         />
       </div>
       <div>
         <label>Email:</label>
-        <input type="email" name="email" placeholder="email" />
+        <input
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={inputChangeHandler}
+        />
       </div>
 
       <button>Submit</button>
