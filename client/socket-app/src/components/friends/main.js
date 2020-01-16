dd0255cd6d75d233610d1650883a4b79f091a599
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-01-16 11:56:31 -0800

    added friend list and main friend form

---
+++ client/socket-app/src/components/friends/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SearchFriendForm } from "./searchFriendForm.js";
+import { FriendList } from "./searchFriendForm";
+
+const FriendsMain = () => {
+  return;
+  <>
+    <SearchFriendForm />
+    <FriendList />
+  </>;
+};
+
+export default FriendsMain;
